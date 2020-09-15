fa02d38faa99a544c6749543d9342d4fbefcc135
Andra <andrathema@gmail.com>
2020-09-15 17:59:16 +0200

    added new map

--- src/Components/MapCovid.js
+++ src/Components/MapCovid.js
@@ -32,8 +32,8 @@ class MapCovid extends Component{
       <div className='map-container'>
         <Map className='map' center={[0, 0]} zoom={3}>
           <TileLayer
-            url='https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png'
-            attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+            url='https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png'
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
             />
           <CumulativeCases />
           <Timeline />
